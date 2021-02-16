c59a7e314a07618ab42ba69dd984eee9c045cfb4
d2a-sveurink <72853442+d2a-sveurink@users.noreply.github.com>
2021-02-16 15:17:20 +0100

    Vektis portaalsets

---
+++ portalen/vektis/vektis_zorgkosten_2018.sql
@@ -0,0 +1,154 @@
+DROP        TABLE IF EXISTS sd_vektis_zorgkosten_2018
+;
+
+CREATE      TABLE sd_vektis_zorgkosten_2018 AS 
+SELECT      v."Kosten Langdurige Ggz"
+,           v."Kosten Huisarts Overig"
+,           v."Kosten Verpleging En Verzorging"
+,           v."Kosten Eerstelijns Ondersteuning"
+,           v."Aantal verzekerdejaren"
+,           v."Aantal BSN"
+,           v."Kosten Hulpmiddelen"
+,           v."Kosten Huisarts Inschrijftarief"
+,           v."Kosten Medisch Specialistische Zorg"
+,           v."Kosten Huisarts Consult"
+,           v."Kosten Grensoverschrijdende Zorg"
+,           v."Kosten Geriatrische Revalidatiezorg"
+,           v."Kosten Generalistische Basis Ggz"
+,           v."Kosten Farmacie"
+,           v."Kosten Eerstelijns Verblijf"
+,           v."Kosten Huisarts Mdz"
+,           v."Kosten Kraamzorg"
+,           v."Kosten Overig"
+,           v."Kosten Paramedische Zorg Overig"
+,           v."Kosten Mondzorg"
+,           v."Kosten Ziekenvervoer Liggend"
+,           v."Kosten Paramedische Zorg Fysiotherapie"
+,           v."Kosten Ziekenvervoer Zittend"
+,           v."Kosten Verloskundige Zorg"
+,           v."Kosten Specialistische Ggz"
+,           v."Geslacht"
+,           v."Leeftijdscategorie"
+,           r."Gemeente"
+,           r."Gemeentecode"
+,           r."Provincie"
+,           r."Landsdeel"
+,           r."Provinciecode"
+,           r."Landsdeelcode"
+,           v."Jaar"
+,           v."Type kosten"
+,           v."Kosten"
+FROM        
+            (           
+            SELECT      * 
+            ,           2018 AS "Jaar"
+            FROM        bi_vektis_2018
+CROSS 
+            JOIN        LATERAL 
+                        (           VALUES 
+                                    (           'Overig'
+                                    ,           "Kosten Overig"
+                                    )           
+                        ,           
+                                    (           'Paramedische Zorg Overig'
+                                    ,           "Kosten Paramedische Zorg Overig"
+                                    )           
+                        ,           
+                                    (           'Ziekenvervoer Zittend'
+                                    ,           "Kosten Ziekenvervoer Zittend"
+                                    )           
+                        ,           
+                                    (           'Ziekenvervoer Liggend'
+                                    ,           "Kosten Ziekenvervoer Liggend"
+                                    )           
+                        ,           
+                                    (           'Kraamzorg'
+                                    ,           "Kosten Kraamzorg"
+                                    )           
+                        ,           
+                                    (           'Verloskundige Zorg'
+                                    ,           "Kosten Verloskundige Zorg"
+                                    )           
+                        ,           
+                                    (           'Generalistische Basis Ggz'
+                                    ,           "Kosten Generalistische Basis Ggz"
+                                    )           
+                        ,           
+                                    (           'Langdurige Ggz'
+                                    ,           "Kosten Langdurige Ggz"
+                                    )           
+                        ,           
+                                    (           'Grensoverschrijdende Zorg'
+                                    ,           "Kosten Grensoverschrijdende Zorg"
+                                    )           
+                        ,           
+                                    (           'Eerstelijns Ondersteuning'
+                                    ,           "Kosten Eerstelijns Ondersteuning"
+                                    )           
+                        ,           
+                                    (           'Geriatrische Revalidatiezorg'
+                                    ,           "Kosten Geriatrische Revalidatiezorg"
+                                    )           
+                        ,           
+                                    (           'Verpleging En Verzorging'
+                                    ,           "Kosten Verpleging En Verzorging"
+                                    )           
+                        ,           
+                                    (           'Medisch Specialistische Zorg'
+                                    ,           "Kosten Medisch Specialistische Zorg"
+                                    )           
+                        ,           
+                                    (           'Farmacie'
+                                    ,           "Kosten Farmacie"
+                                    )           
+                        ,           
+                                    (           'Specialistische Ggz'
+                                    ,           "Kosten Specialistische Ggz"
+                                    )           
+                        ,           
+                                    (           'Huisarts Inschrijftarief'
+                                    ,           "Kosten Huisarts Inschrijftarief"
+                                    )           
+                        ,           
+                                    (           'Huisarts Consult'
+                                    ,           "Kosten Huisarts Consult"
+                                    )           
+                        ,           
+                                    (           'Huisarts Mdz'
+                                    ,           "Kosten Huisarts Mdz"
+                                    )           
+                        ,           
+                                    (           'Huisarts Overig'
+                                    ,           "Kosten Huisarts Overig"
+                                    )           
+                        ,           
+                                    (           'Hulpmiddelen'
+                                    ,           "Kosten Hulpmiddelen"
+                                    )           
+                        ,           
+                                    (           'Mondzorg'
+                                    ,           "Kosten Mondzorg"
+                                    )           
+                        ,           
+                                    (           'Paramedische Zorg Fysiotherapie'
+                                    ,           "Kosten Paramedische Zorg Fysiotherapie"
+                                    )           
+                        ,           
+                                    (           'Eerstelijns Verblijf'
+                                    ,           "Kosten Eerstelijns Verblijf"
+                                    )           
+                        )           AS t
+                        (           "Type kosten"
+                        ,           "Kosten"
+                        )           
+            )           v
+JOIN        bi_vektis_ref_gemeente r
+ON          v."Gemeente" = r."Gemeente_vektis"
+;
+
+ANALYZE sd_vektis_zorgkosten_2018
+;
+
+--GRANT ALL 
+--ON          TABLE sd_vektis_zorgkosten_2018 TO d2aportal
+--;
